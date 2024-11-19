99233853d3d666b699d859839f55fdfffa10a6aa
male-andante <andreadanteprivitera@gmail.com>
2024-11-19 21:06:49 +0100

    realizzato fino a Extra2

--- js/S2P1-scaricabile.js
+++ js/S2P1-scaricabile.js
@@ -4,14 +4,14 @@
  Scrivi un algoritmo per trovare il più grande tra due numeri interi.
 */
 
-let num1 = parseInt(prompt("Inserisci il primo numero"))
+/*let num1 = parseInt(prompt("Inserisci il primo numero"))
 let num2 = parseInt(prompt("Inserisci il secondo numero"))
 
 if (num1 > num2) {
 console.log(`${num1} e' piu' grande di ${num2}`)
 alert(`${num1} e' piu' grande di ${num2}`)
 } else {console.log(`${num2} e' piu' grande di ${num1}`)
-alert(`${num2} e' piu' grande di ${num1}`)}
+alert(`${num2} e' piu' grande di ${num1}`)}*/
 
 /*
 ESERCIZIO 2
@@ -24,7 +24,7 @@ ESERCIZIO 2
   num >= 20 - mostra in console "Huge"
 */
 
-let num = parseInt(prompt("Inserisci un numero"))
+/*let num = parseInt(prompt("Inserisci un numero"))
 
 if (num < 5) {
   console.log("Tiny")
@@ -38,7 +38,7 @@ if (num < 5) {
   console.log("Huge")
 } else {
   console.log("Inserisci un numero valido")
-}
+}*/
 
 //ESERCIZI SUI CICLI: 
 
@@ -46,24 +46,24 @@ if (num < 5) {
   Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8 (suggerimento: ripassa l'uso di "continue").
 */
 
-for ( let i=0; i<=10; i++) {
+/*for ( let i=0; i<=10; i++) {
   if (i===3 || i===8) {
     continue
   } 
   console.log(i)
-  }
+  }*/
 
 /* ESERCIZIO 11
   Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare the il valore corrente sia pari o dispari, e mostrare il risultato in console.
 */
 
-for (let i=0; i<=15; i++) {
+/*for (let i=0; i<=15; i++) {
   if (i % 2 === 0) {
     console.log(`${i} e' pari`)
   } else {
     console.log(`${i} e' dispari`)
   }
-}
+}*/
 
 //ESERCIZI EXTRA NON OBBLIGATORI
 
@@ -71,7 +71,12 @@ for (let i=0; i<=15; i++) {
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*let num1 = parseInt(prompt("Inserisci il primo numero"))
+let num2 = parseInt(prompt("Inserisci il secondo numero"))
+
+if (num1 === 8 || num2 === 8 || num1 + num2 === 8 || num1 - num2 === 8) {
+  console.log("uno dei casi si e' verificato")
+}*/
 
 /* ESERCIZIO EXTRA 2
 Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -79,14 +84,51 @@ C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha d
 Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* ALTERNATIVA 1 
+let totalShoppingCart = /*parseFloat(prompt("Inserisci il totale del carrello")) 45
+let totaleConSpedizione = 0
+let spedizione = 10
+
+if (totalShoppingCart > 50) {
+  totaleConSpedizione = totalShoppingCart
+} else {
+  totaleConSpedizione = totalShoppingCart + spedizione
+}*/
+
+/* ALTERNATIVA 2
+let totalShoppingCart = 45
+let shippingCost
+
+if (totalShoppingCart > 50) {
+  shippingCost = 0
+  console.log("La spedizione e' gratuita")
+} else {
+  shippingCost = 10
+  console.log("La spedizione costa 10 euro")
+}
+let finalCheckout = totalShoppingCart + shippingCost
+console.log(`Il totale da pagare e' di ${finalCheckout} euro`)*/
 
 /* ESERCIZIO EXTRA 3
   Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando, usando l'algoritmo del codice precedente, se le spedizioni siano gratuite oppure no e e calcolando il totale.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let totalShoppingCart = 80
+let shippingCost
+const SCONTOBLACKFRIDAY = 0.2
+let prezzoScontato = totalShoppingCart - (totalShoppingCart* SCONTOBLACKFRIDAY)
+
+if (totalShoppingCart > 50) {
+  shippingCost = 0
+  console.log("La spedizione e' gratuita")
+} else {
+  shippingCost = 10
+  console.log("La spedizione costa 10 euro")
+}
+let finalCheckout = totalShoppingCart + shippingCost
+console.log(`Il totale da pagare e' di ${finalCheckout} euro`)
+
 
 /*  ESERCIZIO EXTRA 4
   Usa un operatore ternaio per assegnare ad una variabile chiamata "gender" i valori "male" o "female".
